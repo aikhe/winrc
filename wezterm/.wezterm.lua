8eb4fe0d8543448c2f4084a6a3a56579ce5a5f16
aikhe <ikeandrie.ro@gmail.com>
2025-09-11 15:13:55 +0800

    feat(wezterm): clean af colorscheme base of lackluster.nvim

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -34,7 +34,7 @@ config.window_padding = {
 }
 
 config.use_fancy_tab_bar = true
-config.enable_kitty_keyboard=true
+config.enable_kitty_keyboard = true
 
 -- TABS
 config.hide_tab_bar_if_only_one_tab = true
@@ -108,9 +108,9 @@ config.keys = {
 		action = act.AdjustPaneSize({ "Right", 5 }),
 	},
 	{
-		key = 'q',
-		mods = 'CTRL|SHIFT',
-		action = wezterm.action.CloseCurrentPane { confirm = true },
+		key = "q",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
 	},
 	{ key = "9", mods = "CTRL", action = act.PaneSelect },
 	{ key = "q", mods = "CTRL", action = act.ShowDebugOverlay },
@@ -145,15 +145,82 @@ table.insert(config.keys, { key = "l", mods = "CTRL|ALT", action = act.ActivateP
 -- For example, changing the color scheme:
 -- config.color_scheme = "Cloud (terminal.sexy)"
 config.colors = {
-	-- background = '#3b224c',
-	-- background = "#181616", -- vague.nvim bg
-	-- background = "#080808", -- almost black
+	-- The default text color
+	foreground = "silver",
+	-- The default background color
+	background = "black",
+
+	-- Overrides the cell background color when the current cell is occupied by the
+	-- cursor and the cursor style is set to Block
+	cursor_bg = "#52ad70",
+	-- Overrides the text color when the current cell is occupied by the cursor
+	cursor_fg = "black",
+	-- Specifies the border color of the cursor when the cursor style is set to Block,
+	-- or the color of the vertical or horizontal bar when the cursor style is set to
+	-- Bar or Underline.
+	cursor_border = "#52ad70",
+
+	-- the foreground color of selected text
+	selection_fg = "black",
+	-- the background color of selected text
+	selection_bg = "#aaaaaa",
+
+	-- The color of the scrollbar "thumb"; the portion that represents the current viewport
+	scrollbar_thumb = "#222222",
+
+	-- The color of the split lines between panes
+	split = "#191919",
+
+	ansi = {
+		"black",
+		"maroon",
+		"green",
+		"#555555",
+		"navy",
+		"purple",
+		"teal",
+		"silver",
+	},
+	brights = {
+		"grey",
+		"#deeeed",
+		"#ffaa88",
+		"#7788aa",
+		"blue",
+		"fuchsia",
+		"#708090",
+		"white",
+	},
+
+	-- Arbitrary colors of the palette in the range from 16 to 255
+	indexed = { [136] = "#af8700" },
+
+	-- Since: 20220319-142410-0fcdea07
+	-- When the IME, a dead key or a leader key are being processed and are effectively
+	-- holding input pending the result of input composition, change the cursor
+	-- to this color to give a visual cue about the compose state.
+	compose_cursor = "orange",
+
+	-- Colors for copy_mode and quick_select
+	-- available since: 20220807-113146-c2fee766
+	-- In copy_mode, the color of the active text is:
+	-- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
+	-- 2. selection_* otherwise
+	copy_mode_active_highlight_bg = { Color = "#000000" },
+	-- use `AnsiColor` to specify one of the ansi color palette values
+	-- (index 0-15) using one of the names "Black", "Maroon", "Green",
+	--  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
+	-- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
+	copy_mode_active_highlight_fg = { AnsiColor = "Black" },
+	copy_mode_inactive_highlight_bg = { Color = "#52ad70" },
+	copy_mode_inactive_highlight_fg = { AnsiColor = "White" },
+
+	quick_select_label_bg = { Color = "peru" },
+	quick_select_label_fg = { Color = "#ffffff" },
+	quick_select_match_bg = { AnsiColor = "Navy" },
+	quick_select_match_fg = { Color = "#ffffff" },
+
 	background = "#000000", -- dark purple
-	-- background = "#020202", -- dark purple
-	-- background = "#17151c", -- brighter purple
-	-- background = "#16141a",
-	-- background = "#0e0e12", -- bright washed lavendar
-	-- background = 'rgba(59, 34, 76, 100%)',
 	cursor_border = "#ffffff",
 	-- cursor_fg = "#281733",
 	cursor_bg = "#ffffff",
@@ -163,7 +230,7 @@ config.colors = {
 		background = "rgba(0,0,0,0%)",
 		active_tab = {
 			bg_color = "rgba(0,0,0,0%)",
-			fg_color = "#ffffff",
+			fg_color = "#aaaaaa",
 			intensity = "Normal",
 			underline = "None",
 			italic = false,
@@ -171,7 +238,7 @@ config.colors = {
 		},
 		inactive_tab = {
 			bg_color = "rgba(0,0,0,0%)",
-			fg_color = "#eeeeee",
+			fg_color = "#555555",
 			intensity = "Normal",
 			underline = "None",
 			italic = false,
